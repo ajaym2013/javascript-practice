e980414ce3a9668bc8e8abbd26e357ad6c2798c7
Ajay Maurya <168641663+ajaym2013@users.noreply.github.com>
2024-06-10 10:03:44 +0000

    learn data types

---
+++ firstclass/data-types.js
@@ -0,0 +1,20 @@
+// primitive
+/*
+string
+number
+boolean
+null
+undefined
+symbol
+bigInt
+*/
+const firstname = "ajay"
+const age = 20
+const score = 22.0
+let isLoggedIn = false
+const nodeVersion = null
+let bankBalance = undefined
+const buttonId = symbol("id")
+const aLongNumber = 233535425456n
+
+//reference (non primitive)
