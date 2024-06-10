c0dec7248305fd70fd84e9772291fd50abdb6178
Ajay Maurya <168641663+ajaym2013@users.noreply.github.com>
2024-06-10 13:00:37 +0000

    learn operators

---
+++ firstclass/operator.js
@@ -0,0 +1,29 @@
+// Arthmatic operator
+
+// 5+5
+// 10-5
+// 5*5
+// 10/2
+// 2**2 => 2*2*2
+// 10%3
+
+let valueone = 3
+valueone++
+// its mean valueone + 1
+
+const n1 = "2"
+const n2 = "4"
+
+const n3 = n1 + n2
+
+console.log(n3);
+
+// Assignment Operators
+let valuetwo = 3
+valuetwo += 10
+
+//compersion Operators
+let pw = 3
+let pwskills = "3"
+
+console.log(pw === pwskills);
