8f9959cd48828184d54423dc7cb15e6daedd6685
Ajay Maurya <168641663+ajaym2013@users.noreply.github.com>
2024-06-11 11:01:23 +0000

    learn strings

---
+++ firstclass/numbers-strings.js
@@ -0,0 +1,23 @@
+const first = 'hello'
+const last = "world"
+
+console.log(first + ' ' + last);
+
+//bactics written that
+console.log(`my first ${first} is my last ${last}`)
+
+const username = new String('ajay maurya')
+console.log(username[0]);
+console.log(username.length);
+console.log(username.charAt(0));
+console.log(username.substring(0, 4));
+
+
+let email = "ajay@gmail.com"
+console.log(email.trim());
+
+let emailtwo = "ajay@gmail.live"
+console.log(emailtwo.replace("ajay", "maurya"));
+console.log(email.includes("ajay"));
+console.log(emailtwo.toUpperCase);
+
