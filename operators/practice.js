34028730d0d9432579263738031285c970852a82
Ajay Maurya <168641663+ajaym2013@users.noreply.github.com>
2024-09-06 10:52:38 +0000

    if else learn

---
+++ operators/practice.js
@@ -0,0 +1,24 @@
+let star = "3";
+let startIntNumber = parseInt(star)
+
+if (startIntNumber == 1)
+{
+    console.log('poor');
+} else if (startIntNumber == 2) 
+{
+    console.log('emm, OK');
+}else if (startIntNumber == 3) 
+    {
+        console.log('average');
+    }
+    else if (startIntNumber == 4) 
+        {
+            console.log('good');
+        }
+        else if (startIntNumber == 5) 
+            {
+                console.log('greet');
+            } else 
+{
+    console.log('i dont know rating');
+}
