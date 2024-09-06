8305d580129d75658017b644e7b4fe01cc6073d7
Ajay Maurya <168641663+ajaym2013@users.noreply.github.com>
2024-09-06 11:20:03 +0000

    learn switch case

---
+++ operators/switch.js
@@ -0,0 +1,41 @@
+// switch(expression)
+// {
+//     case vlaue1:
+//         statement1;
+//         statement2;
+//         break;
+     
+//     case value2:
+//         statement3;
+//         statement4;    
+//         break;
+// }
+
+let star = "9";
+let intStar = parseInt(star);
+
+switch (intStar)
+{
+    case 1:
+        console.log("poor");
+        break;
+
+    case 2:
+        console.log("emm, ok");
+        break;
+
+    case 3:
+        console.log("average");
+        break;
+
+     case 4:
+        console.log("good");
+        break;  
+
+     case 4:
+         console.log("greet");
+         break;          
+         
+    default:
+        console.log('what is rating for');   
+}
