1da7c3234806c671bb3b0e4c9a88b32618034c07
Ajay Maurya <168641663+ajaym2013@users.noreply.github.com>
2024-09-10 10:24:55 +0000

    learn for loop

---
+++ control/loop1.js
@@ -0,0 +1,20 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for ([initialExpression/decleration] ; [condition]; [changer])
+// {
+//     //body
+
+// }
+
+let userScore = 10;
+for ( userScore ; userScore >= 0; userScore--)
+{
+    if (userScore == 5)
+    {
+        continue
+    }
+console.log(`userscore is ${userScore}`);
+}
